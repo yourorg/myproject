28fadc8398dfbeb575906d62538a3ae5c09970d9
Yourself <yourself.yourorg@gmail.com>
2016-02-17 17:38:09 +0000

    Add script to clone packages and symlink to them, but [ci skip].

---
+++ packages/managed_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Use the array 'MANAGED_PACKAGES' to specify the packages to clone.
+# 
+#    ORG_NAME  |  PKG_NAME  |  SUB_DIR  
+#
+MANAGED_PACKAGES=(
+  "dude-awap|dummy|thirdparty/dudeAwap" \
+  "yourorg|mypackage|yourself"
+);
+
